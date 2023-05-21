71d5495da533c92c664100da153276915a2485dd
M-x-7 <xrg2580123@gmail.com>
2023-05-21 12:46:04 +0800

    v3_20230521

--- OKX/Main.py
+++ OKX/Main.py
@@ -6,25 +6,20 @@ import time
 import base64
 import hmac
 import hashlib
-import os
-
-
-import GUI
-import settings
-
 
 # 檢查keydata內容是否為空
-def checkData():
+def Check_keydata():
   with open("keydata.json", "r") as f:
-      data = json.load(f)
-      if not data["apikey"] or not data["secretkey"] or not data["passwd"]:
-        GUI.createGUI()
+    data = json.load(f)
+    if not data["apikey"] or not data["secretkey"] or not data["passwd"] or not data["flag"]:
+      import inputKey
+      inputKey.keyGUI()
     
 # 時間戳獲取功能(交易所系統時間)
 def sysTime():
   # 從交易所獲取時間
   systemTime = int(requests.get("https://www.okx.com/api/v5/public/time").json()["data"][0]["ts"])
-  # ISO格式時間戳
+  # 轉換為ISO格式時間戳
   isoTimestamp = datetime.datetime.fromtimestamp(systemTime/1000).isoformat()[:-3]+"Z"        
   return isoTimestamp
 
@@ -32,16 +27,18 @@ def sysTime():
 def utcTime():
   # 獲取UTC時間
   utcTime = datetime.datetime.utcnow()
-  # ISO格式時間戳
+  # 轉換為ISO格式時間戳
   isoTimestamp = utcTime.isoformat()[:-3]+"Z"        
   return isoTimestamp
 
 # API連線功能
-def apiSet(apikey, secretkey, passwd, path):
+def apiSet(apikey, secretkey, passwd, path, flag):
   # 時間及路徑設置
-  method = "GET"                                                
-  timestamp = utcTime()                                       
-  prehashString = timestamp + method + path                     # 合成預置字串
+  method = "GET"
+  timestamp = utcTime()
+
+  # 合成預置字串
+  prehashString = timestamp + method + path
 
   # 預置字串用密鑰HMAC-SHA256加密，再經過Base64編碼
   signature = base64.b64encode(hmac.new(secretkey.encode(), prehashString.encode(), hashlib.sha256).digest()).decode()
@@ -53,94 +50,27 @@ def apiSet(apikey, secretkey, passwd, path):
     "OK-ACCESS-TIMESTAMP": timestamp,
     "OK-ACCESS-PASSPHRASE": passwd,
     "Content-Type": "application/json",
-    "x-simulated-trading": "1"                                  #此為因應開發使用模擬盤API，實務情況務必刪除
+    "x-simulated-trading": flag
   }
   # 發送API請求
   response = requests.get("https://www.okx.com" + path, headers=headers)
   # 返回Json格式資料
   return response.json()
 
+#--------------------
+#     主程式開始
+#--------------------
 
-# 設定預設選項列表和初始化目前選擇的項目
-options = ['Balance', 'BTC price', 'ETH price']
-current_option = 0
-
-# 顯示目前選項和其它選項
-def show_options():
-  for i in range(len(options)):
-    if i == current_option:
-      print('>> ' + options[i])
-    else:
-      print('   ' + options[i])
-
-# 導入API金鑰和密鑰
-checkData()
-
+# 檢查或導入金鑰
+Check_keydata()
 with open("keydata.json", "r") as f:
   data = json.load(f)
   apikey = data["apikey"]
   secretkey = data["secretkey"]
   passwd = data["passwd"]
+  flag = data["flag"]
 
-print(secretkey)
-print(apikey)
-print(passwd)
-
-show_options()
-#程式迴圈
-
-while True:
-  while True:
-        try:
-            time.sleep(0.15)
-
-            # 按下上方向鍵
-            if keyboard.is_pressed('up'):
-                os.system('cls')
-                if current_option == 0:
-                    current_option = 2
-                else:
-                    current_option -= 1
-                show_options()
-
-            # 按下下方向鍵
-            elif keyboard.is_pressed('down'):
-                os.system('cls')
-                if current_option == 2:
-                    current_option = 0
-                else:
-                    current_option += 1
-                show_options()
-
-            # 如果按Enter鍵，確認選擇並退出
-            elif keyboard.is_pressed('enter'):
-                break
-
-        # 捕捉並處理keyboard.interrupt_exception例外
-        except keyboard.KeyboardInterrupt:
-            break
-        
+# 程式迴圈
 
 
 
-  while True:
-    # 路徑隨選擇導入
-    path = ''
-    if current_option == 0:
-      path = settings.paths[0]
-    elif current_option == 1:
-      path = settings.paths[1]
-    elif current_option == 2:
-      path = settings.paths[2]
-
-    #調用API，並根據選項輸出內容
-    Temp = apiSet(apikey, secretkey, passwd, path)
-    if current_option == 0:
-        type = "USD Valuation"
-        values = Temp["data"][0]["totalEq"][:-10]
-        
-    else :
-        type = Temp["data"][0]["instId"]
-        values = Temp["data"][0]["last"]
-    print(type +": " +values)
-    break
--- OKX/inputKey.py
+++ OKX/inputKey.py
@@ -6,60 +6,71 @@ window = None
 keyEntry = None
 skeyEntry = None
 passwdEntry = None
+flag = None
 
-
-# 建立視窗功能
-def createGUI():
-    global window, keyEntry, skeyEntry, passwdEntry
+# 金鑰輸入視窗
+def keyGUI():
+    global window, keyEntry, skeyEntry, passwdEntry, flag
     # 建立主視窗
     window = tk.Tk()
     window.title("API載入")
-    window.geometry("400x200")
+    window.geometry("300x180")
 
     # API Key(金鑰) 輸入框
     keyLabel = tk.Label(window, text="金鑰:")
-    keyLabel.pack()
+    keyLabel.pack(pady = 1)
     keyEntry = tk.Entry(window)
     keyEntry.pack()
 
     # Secret Key(密鑰) 輸入框
     skeyLabel = tk.Label(window, text="密鑰:")
-    skeyLabel.pack()
+    skeyLabel.pack(pady = 1)
     skeyEntry = tk.Entry(window, show="*")
     skeyEntry.pack()
 
     # Password(密碼) 輸入框
-    passwdLabel = tk.Label(window, text="API Password:")
-    passwdLabel.pack()
+    passwdLabel = tk.Label(window, text="密碼:")
+    passwdLabel.pack(pady = 1)
     passwdEntry = tk.Entry(window, show="*")
     passwdEntry.pack()
 
-    # 送出按鈕
-    submit_button = tk.Button(window, text="儲存", command=submitKey)
-    submit_button.pack()
+    # 父容器框架(排版按鈕和勾選盒)
+    frame = tk.Frame(window)
+    frame.pack(side = "bottom", pady = 5)
+
+    # 送出按鈕(子部件)
+    submit_button = tk.Button(frame, text="儲存", command=submitKey)
+    submit_button.grid(row=0, column=0)
+
+    # 模擬盤勾選框(子部件)
+    flag = tk.IntVar()
+    checkbox = tk.Checkbutton(frame, text="模擬盤", variable=flag)
+    checkbox.grid(row=0, column=1)
 
     # 啟動主迴圈
     window.mainloop()
 
-# 訊息送出功能
+# 送出功能
 def submitKey():
-    global window, keyEntry, skeyEntry, passwdEntry
+    global window, keyEntry, skeyEntry, passwdEntry, flag
     # 取得在輸入框裡的內容
     apikey = keyEntry.get()
     secretkey = skeyEntry.get()
     passwd = passwdEntry.get()
-
+    flag = flag.get()
     # 建立字典
     Data = {
         "apikey": apikey,
         "secretkey": secretkey,
-        "passwd": passwd
+        "passwd": passwd,
+        "flag" : flag
     }
 
     # 將資料儲存為 JSON 格式
-    with open("keydata.json", "w") as json_file:
-        json.dump(Data, json_file)
+    with open("keydata.json", "w") as f:
+        json.dump(Data, f)
 
     # 關閉視窗
     window.destroy()
 
+keyGUI()
